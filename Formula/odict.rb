e33e5be3f00dd9925482e7260bf57393a83f7f37
Tyler Nickerson <nickersoft@gmail.com>
2021-01-17 17:43:52 -0800

    updated to 1.4

--- Formula/odict.rb
+++ Formula/odict.rb
@@ -1,13 +1,14 @@
 class Odict < Formula
   desc "Command-line utility for the ODict dictionary format"
   homepage "https://odict.org"
-  url "https://github.com/odict/odict/archive/1.3.tar.gz"
-  sha256 "6a0735789b83f875983b385f0762b9e42a47c356cf4da6651d913f05737d85e4"
+  url "https://github.com/odict/odict/archive/1.4.tar.gz"
+  sha256 "81bab611bcf93b6b8baac2e11fa6a5aa7ca907509bd5ad654fe58c2bfcafac1d"
   depends_on "go" => :build
+  depends_on "bazel" => :build 
 
   def install
-    system "sh", "scripts/build.sh"
-    bin.install "bin/odict" => "odict"
+    system "bazel", "build", "cli"
+    bin.install "bazel-bin/cli/odict" => "odict"
   end
 
   test do
