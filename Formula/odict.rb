a296a5951618c3a15725957f20f130558e54ba14
Tyler Nickerson <nickersoft@gmail.com>
2021-02-18 14:10:22 -0800

    chore(*): updated Github URL

--- Formula/odict.rb
+++ Formula/odict.rb
@@ -1,7 +1,7 @@
 class Odict < Formula
   desc "Command-line utility for the ODict dictionary format"
   homepage "https://odict.org"
-  url "https://github.com/odict/odict/archive/1.4.3.tar.gz"
+  url "https://github.com/TheOpenDictionary/odict/archive/1.4.3.tar.gz"
   sha256 "9157b79ce39ae201d40ad5aa3a2692cc9b72233c83f041f6552ffce8b4b15d60"
   depends_on "go" => :build
   depends_on "bazel" => :build 
