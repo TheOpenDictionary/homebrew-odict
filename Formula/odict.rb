7d2e70dce01e8effa2d97c5ce0c2a0ef885d89ca
Tyler Nickerson <nickersoft@gmail.com>
2021-05-24 22:42:13 -0700

    chore(*): upgrade version

--- Formula/odict.rb
+++ Formula/odict.rb
@@ -1,8 +1,8 @@
 class Odict < Formula
   desc "Command-line utility for the ODict dictionary format"
   homepage "https://odict.org"
-  url "https://github.com/TheOpenDictionary/odict/archive/1.4.3.tar.gz"
-  sha256 "9157b79ce39ae201d40ad5aa3a2692cc9b72233c83f041f6552ffce8b4b15d60"
+  url "https://github.com/TheOpenDictionary/odict/archive/1.4.5.tar.gz"
+  sha256 "50ef737cce963025f4076bcb52f2c82fc64c1ab8b53747e80fa36a382c696a44"
   depends_on "go" => :build
   depends_on "bazel" => :build 
 
